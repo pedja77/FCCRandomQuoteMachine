eb3373a440b9e4620bbf37387fc985a352266eb9
Pedja Smigeljski <pedja77@stcable.net>
2017-01-09 18:32:51 +0100

    Added some styles, frameset to choose categories and button to display next fortune

--- script.js
+++ script.js
@@ -1,11 +1,38 @@
 window.onload = function(){
 
-  function getFortune() {
-    let randCategory = Object.keys(fortunes)[Math.floor(Object.keys(fortunes).length * Math.random())];
-    let randIndex = Math.floor(fortunes[randCategory].length * Math.random());
-    return fortunes[randCategory][randIndex];
+  function categories () {
+    // Get selected categories nad return them in array
+    return Array.from(document.getElementsByClassName("category"))
+                .filter(e => e.checked === true)
+                .map(e => e.id);//Object.keys(fortunes);
   }
-  let quote = document.getElementById('quote');
-  quote.innerHTML = getFortune();
-  //document.write(randFortune);
+
+  //let loaded = new Object(fortunes);
+  let selectedCategories = categories();
+
+  function getFortune() {
+    // Get random fortune from selected categories
+    if (selectedCategories.length === 0)
+      return "You must choose at least one category!!!";
+    let randCategory = selectedCategories[Math.floor(selectedCategories.length * Math.random())];
+    let randIndex = Math.floor(fortunes[randCategory].length * Math.random());
+    return randCategory + '\n' + fortunes[randCategory][randIndex];
+  }
+
+  function showFortune () {
+    // Show fortune from one of selected categories. All categories are selected on page load
+    let quote = document.getElementById('quote');
+    quote.innerHTML = getFortune();
+    // Following code is for testing purposes
+    let fset = categories();
+    console.log(fset);
+    console.log(Object.keys(fortunes));
+  }
+
+  // Show fortune on page load
+  showFortune();
+
+  // Event listeners
+  document.getElementById("next-fortune").addEventListener("click", showFortune);
+  document.getElementById("categories").addEventListener("click", () => selectedCategories = categories());
 }
